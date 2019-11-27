5cf7912cfa8c65294b2cd98d8bab4faca46092dd
laitsky <vincent.diamond15@gmail.com>
2019-11-27 07:25:22 +0700

    new component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { ProfilKelompokComponent } from './profil-kelompok/profil-kelompok.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    UserListComponent,
+    UserDetailComponent,
+    ProfilKelompokComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profil-kelompok/profil-kelompok.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-kelompok',
+  templateUrl: './profil-kelompok.component.html',
+  styleUrls: ['./profil-kelompok.component.css']
+})
+export class ProfilKelompokComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
