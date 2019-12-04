8652a75834a137d8c8d9d2b33faa324900569363
lianaa-li <55238298+lianaa-li@users.noreply.github.com>
2019-12-04 11:03:16 +0700

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { SHA512 } from 'crypto-js';
 declare var particlesJS: any;
 @Component({
   selector: 'app-login',
@@ -8,10 +11,36 @@ declare var particlesJS: any;
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json " })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     particlesJS.load('particles-js', 'assets/LoginParticles.json', null);
+  }
+
+  login(){
+    this.http.post('http://umn-pti2019.herokuapp.com/api/login', {
+      "user_name": this.username,
+      "password": SHA512(this.password).toString()
+    }, this.httpOptions
+    ).subscribe(
+      (response) => {
+        if(response['token'] != null){
+          localStorage.setItem('uas-pti-token', response['token']);
+          this.router.navigate(['home']);
+        }
+      },
+      (error) => alert(error.error.message)
+    )
   }
 
 }
