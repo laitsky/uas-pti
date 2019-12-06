72522b2ab4445eaf54e6cb36663d8a12cbe2b96c
lianaa-li <liana1@student.umn.ac.id>
2019-12-07 01:31:26 +0700

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {transition, trigger, query, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [style({ opacity: 0 })],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+           [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'uas-pti';
