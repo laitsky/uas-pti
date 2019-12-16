21adc53faa978c5b41907df786b918fb64925ebe
Vincent Diamond <vincent.diamond15@gmail.com>
2019-12-16 19:17:21 +0700

    update: grouping mahasiswa done

--- src/app/grouping-mahasiswa/grouping-mahasiswa.component.ts
+++ src/app/grouping-mahasiswa/grouping-mahasiswa.component.ts
@@ -10,16 +10,43 @@ import { MahasiswaList } from "../_shared/models/mahasiswa-list";
 export class GroupingMahasiswaComponent implements OnInit {
   public mahasiswa: MahasiswaList;
   kelompokA: Array<any> = [];
+  kelompokB: Array<any> = [];
   constructor(private pelayan: PelayanApiService) { }
 
   ngOnInit() {
     this.pelayan.getAllMahasiswa().subscribe(result => {
       this.mahasiswa = result;
     });
+    document.getElementById("defaultTab").click();
   }
 
-  add(mhs_nama: string, mhs_nim: string) {
+  addToKelA(mhs_nama: string, mhs_nim: string) {
     this.kelompokA.push({mhs_nama, mhs_nim});
     console.log(this.kelompokA);
+  }
+  addToKelB(mhs_nama: string, mhs_nim: string) {
+    this.kelompokB.push({mhs_nama, mhs_nim});
+    console.log(this.kelompokB);
+  }
+
+  openTab(evt: any, tabName: string) {
+      // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
   }
 }
